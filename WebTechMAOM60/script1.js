cf807fc1289d514ce065a9da02fbc772e3b36094
Adam <petroadam02@gmail.com>
2024-05-01 19:08:40 +0200

    maraeleg

--- WebTechMAOM60/script1.js
+++ WebTechMAOM60/script1.js
@@ -2,3 +2,16 @@ document.getElementById("scrollButton").addEventListener("click", function() {
     var targetElement = document.getElementById("targetElement");
     targetElement.scrollIntoView({ behavior: 'smooth' });
 });
+
+
+//BECSÚSZÓS
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        var tableRows = document.querySelectorAll(".slide-in td");
+        tableRows.forEach(function(row) {
+            if (row.getBoundingClientRect().top < window.innerHeight) {
+                row.classList.add("slide-in-animate");
+            }
+        });
+    });
+});
