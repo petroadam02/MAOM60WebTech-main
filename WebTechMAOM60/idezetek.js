7e2b0e36a1f68d7b9c19b7272d19f6f723aef3da
Adam <petroadam02@gmail.com>
2024-05-06 01:06:36 +0200

    footer minden

--- WebTechMAOM60/idezetek.js
+++ WebTechMAOM60/idezetek.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     // Idézetek tömbje
     var quotes = [
+        "Az igazi fejlődés az akadályok legyőzéséből születik.",
         "A jövődet nem te írod, de a szokásaidat igen, amelyek majd felépítik.",
         "Egyetlen módja van annak, hogy megtudd, képes vagy-e valamire: ha megpróbálod.",
         "Az idő halad. Szóval, bármit is akarsz csinálni, tedd meg most. Ne várj vele!",
         "Nem szentírás, hogy a kis embernek meg kell hátrálnia a nagy feladat előtt.",
         "Az edzés nem csak fizikai erőfeszítés, hanem mentális kitartás is.",
-        "Az elszántság az út kezdetét jelenti, a kitartás pedig a cél eléréséhez vezet.",
-        "Az igazi fejlődés az akadályok legyőzéséből születik."
+        "Az elszántság az út kezdetét jelenti, a kitartás pedig a cél eléréséhez vezet."
     ];
 
     // első idézet
