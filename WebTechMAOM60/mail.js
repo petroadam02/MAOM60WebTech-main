e25d89998d6b6825f33ab349e225a11020e76309
Adam <petroadam02@gmail.com>
2024-05-10 22:22:28 +0200

    form kész

--- WebTechMAOM60/mail.js
+++ WebTechMAOM60/mail.js
@@ -1,17 +1,19 @@
-function sendMail(){
-    let parms ={
-        name : document.getElementById('name').value,
-        email : document.getElementById('email').value,
-        subject : document.getElementById('subject').value,
-        message : document.getElementById('message').value
-    }
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Az alapértelmezett űrlap küldési művelet megakadályozása
 
+    // Az űrlap mezőinek értékeinek ellenőrzése
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
 
-    if (name == "" || email == "" || subject == "" || message == "") {
-        alert("Minden mezőt ki kell tölteni!");
-        return false;
+    if (name && email && subject && message) {
+        // Ha minden mező ki van töltve, akkor alertet jelenítünk meg
+        alert("Az üzenet elküldve!");
+        // További műveletek, például az űrlap elküldése a szerverre
+        // Itt lehetőség van az AJAX hívások vagy más műveletek végrehajtására
+    } else {
+        // Ha bármelyik mező nincs kitöltve, akkor figyelmeztető üzenetet jelenítünk meg
+        alert("Kérlek, töltsd ki az összes mezőt!");
     }
-    else{
-        emailjs.send("service_lg1oysr","template_18fvvwy",parms).then(alert("Az üzenetet elküldtük!"))
-    }
-}
+});
