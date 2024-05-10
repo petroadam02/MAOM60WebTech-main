84dacb07337cc8151d4335992903c2f6c338083f
Adam <petroadam02@gmail.com>
2024-05-10 19:12:10 +0200

    egybejson

--- WebTechMAOM60/peppa.js
+++ WebTechMAOM60/peppa.js
@@ -1,15 +1,11 @@
-$(document).ready(function(){
-    $.ajax({
-        url: 'peppa.json',
-        dataType: 'json',
-        success: function(data){
-            var table = $('#peppaTable tbody');
-            $.each(data, function(index, item){
-                table.append('<tr><td>' + item.nev + '</td><td>' + item.kor + '</td><td>' + item.spec + '</td><td>' + item.tapasztalat + " év" + '</td></tr>');
-            });
-        },
-        error: function(){
-            alert('Hiba a JSON fájl betöltése közben.');
-        }
-    });
+$.getJSON('edzok.json', function (data) {
+    var table = '<table><tr><th>Név</th><th>Kor</th><th>Specializáció</th><th>Tapasztalat</th></tr>';
+    for (var i = 0; i < data["edzok"].length; i++) {
+        var edzo = data["edzok"][i];
+        table += '<tr><td>' + edzo.nev + '</td><td>' + edzo.kor + '</td><td>' + edzo.spec + '</td><td>' + edzo.tapasztalat + '</td></tr>';
+    }
+    table += '</table>';
+    $('#edzokTable').html(table); // A táblázatot a 'edzokTable' div-be helyezzük
+}).fail(function () {
+    console.error('Hiba történt a JSON fájl betöltése közben.');
 });
