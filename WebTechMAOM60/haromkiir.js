c2c13804ef075be461521f8def99912a5500ba88
Adam <petroadam02@gmail.com>
2024-05-10 21:59:58 +0200

    json kész

--- WebTechMAOM60/haromkiir.js
+++ WebTechMAOM60/haromkiir.js
@@ -1,8 +1,8 @@
 $.getJSON('edzok.json', function (data) {
-    var table = '<table><tr><th>Név</th><th>Kor</th><th>Specializáció</th><th>Tapasztalat</th></tr>';
+    var table = '<table><tr><th style="background-color: #11140071; padding: 8px; border-bottom: 1px solid #bde608;color: #bde608;">Név</th><th style="background-color: #11140071; padding: 8px; border-bottom: 1px solid #bde608;color: #bde608;">Kor</th><th style="background-color: #11140071; padding: 8px; border-bottom: 1px solid #bde608;color: #bde608;">Specializáció</th><th style="background-color: #11140071; padding: 8px; border-bottom: 1px solid #bde608;color: #bde608;">Tapasztalat</th></tr>';
     for (var i = 0; i < data["edzok"].length; i++) {
         var edzo = data["edzok"][i];
-        table += '<tr><td>' + edzo.nev + '</td><td>' + edzo.kor + '</td><td>' + edzo.spec + '</td><td>' + edzo.tapasztalat + '</td></tr>';
+        table += '<tr><td style="padding: 8px; border-bottom: 1px solid #bde608;color: #bde608;">' + edzo.nev + '</td><td style="padding: 8px; border-bottom: 1px solid #bde608;color: #bde608;">' + edzo.kor + '</td><td style="padding: 8px; border-bottom: 1px solid #bde608;color: #bde608;">' + edzo.spec + '</td><td style="padding: 8px; border-bottom: 1px solid #bde608;color: #bde608;">' + edzo.tapasztalat + '</td></tr>';
     }
     table += '</table>';
     $('#edzokTable').html(table); // A táblázatot a 'edzokTable' div-be helyezzük
